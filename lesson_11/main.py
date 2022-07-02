e7fa7cbc76d8f93284d8223b375f41deec86290e
Ivan <hakencontinuum1@gmail.com>
2022-07-02 23:35:30 +0200

    added second code

--- lesson_11/main.py
+++ lesson_11/main.py
@@ -1,6 +1,23 @@
+#MANUAL 11
+
+# FIRST CODE
 class Auto:
     @staticmethod
     def get_class_info():
         print('Детальная информация о классе')
 
 Auto.get_class_info()
+
+# SECOND CODE
+class MyClass:
+    @staticmethod
+    def on_sum_1(param_1, param_2):
+        return param_1 + param_2
+
+    def on_sum_2(self, param_1, param_2):
+        return param_1 + param_2
+
+    def on_sum_3(self, param_1, param_2):
+        return MyClass.on_sum_1(param_1, param_2)
+
+print(MyClass.on_sum_1(20, 30))
